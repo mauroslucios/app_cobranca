a8e87f0a1a54e5636cc8ad25aa209789dc1e5b21
mauroslucios <mauroslucios@gmail.com>
2022-01-28 14:44:50 -0300

    application.properties adicionado H2

--- src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
+++ src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
@@ -1,21 +1,37 @@
 package com.mauroslucios.cobranca.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.mauroslucios.model.Titulo;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @Controller
 @Api(value="APP de controle de cobranças")
 @CrossOrigin(origins = "*")
-@RequestMapping("titulos/")
+@RequestMapping("/titulos")
 public class TituloController {
 
 	
-	@GetMapping("novo")
+	@GetMapping("/buscar")
+	@ApiOperation(value="Busca um titulo no banco")
 	public String novo() {
 		return "CadastroTitulo";
 	}
+	
+	@ApiOperation(value="Salva um título no banco")
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/cadastrar")
+	public String salvar(Titulo titulo) {
+		return "CadastroTitulo";
+	}
+	
+	
 }
--- src/main/java/com/mauroslucios/model/Titulo.java
+++ src/main/java/com/mauroslucios/model/Titulo.java
@@ -4,10 +4,14 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -39,12 +43,14 @@ public class Titulo {
 	
 	@NotBlank
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
 	@NotBlank
 	private BigDecimal valor;
 	
 	@NotBlank
+	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
 	
 }
