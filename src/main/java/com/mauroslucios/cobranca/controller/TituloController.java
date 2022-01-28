cb736052c9f9ed48815e448957c25c686e6abf4b
mauroslucios <mauroslucios@gmail.com>
2022-01-28 20:24:17 -0300

    Alert cadastro realizado com suscesso

--- src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
+++ src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mauroslucios.cobranca.model.Titulo;
 import com.mauroslucios.cobranca.service.TituloService;
@@ -22,10 +23,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TituloController {
 
-	
 	private TituloService tituloService;
 	
-	@GetMapping("/buscar")
+	@GetMapping("/novo")
 	@ApiOperation(value="Busca um titulo no banco")
 	public String novo() {
 		return "CadastroTitulo";
@@ -34,9 +34,11 @@ public class TituloController {
 	@ApiOperation(value="Salva um título no banco")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/cadastrar")
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		tituloService.cadastrar(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem","Título salvo com sucesso!");
+		return mv;
 	}
 	
 	
