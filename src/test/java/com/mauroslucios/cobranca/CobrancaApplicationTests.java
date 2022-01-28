14e69fff237074c169b69277b8b497ade26197b3
mauroslucios <mauroslucios@gmail.com>
2022-01-28 08:19:35 -0300

    Projeto inicial

---
+++ src/test/java/com/mauroslucios/cobranca/CobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauroslucios.cobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
