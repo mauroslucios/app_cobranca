94e99ec60f22f517475cf472e154e57e73ac0199
mauroslucios <mauroslucios@gmail.com>
2022-02-04 22:27:06 -0300

    Adicionando addFlashAttribute para manter dados no formulário

--- src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
+++ src/main/java/com/mauroslucios/cobranca/controller/TituloController.java
@@ -3,17 +3,15 @@ package com.mauroslucios.cobranca.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mauroslucios.cobranca.enuns.StatusTitulo;
 import com.mauroslucios.cobranca.model.Titulo;
@@ -32,8 +30,8 @@ public class TituloController {
 
 	private TituloService tituloService;
 	
-	@GetMapping("/novo")
-	@ApiOperation(value="Adicona um titulo no banco")
+	@RequestMapping("/novo")
+	@ApiOperation(value="Exibe a view de novo título")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject(new Titulo());
@@ -41,19 +39,19 @@ public class TituloController {
 	}
 	
 	@ApiOperation(value="Salva um título no banco")
-	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping("/cadastrar")
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		if(errors.hasErrors()) {
 			return mv;
 		}
 		tituloService.cadastrar(titulo);
-		mv.addObject("mensagem","Título salvo com sucesso!");
-		return mv;
+		ModelAndView mv2 = new ModelAndView("redirect:/titulos/novo");
+		attributes.addFlashAttribute("mensagem","Título salvo com sucesso!");
+		return mv2;
 	}
 	
-	@GetMapping("/pesquisa")
+	@RequestMapping("/pesquisa")
 	@ApiOperation(value="Busca um titulo no banco")
 	public ModelAndView pesquisa() {
 		List<Titulo> todosTitulos = tituloService.pesquisar();
