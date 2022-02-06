2e4b6238860ad3b53c253444d48a5f4c2e71f3f7
mauroslucios <mauroslucios@gmail.com>
2022-02-05 21:15:08 -0300

    Finalizado exclusão de título

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -17,7 +17,10 @@ $(document).ready(function() {
 });
 $('#myModal').on('show.bs.modal', function(event){
 		var button = $(event.relatedTarget);
+		
 		var codigoTitulo = button.data('codigo');
+		var descricaoTitulo = button.data('descricao');
+		
 		var modal = $(this);
 		var form = modal.find('form');
 		var action = form.attr('action');
@@ -25,4 +28,9 @@ $('#myModal').on('show.bs.modal', function(event){
 			action += '/';
 		}
 		form.attr('action', action + codigoTitulo);
+		
+		modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong> ' + descricaoTitulo + '</strong>?');
+		
+		
+		
 });
